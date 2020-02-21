0edc5b8930bfa5a59d6beca87661067398163a67
Eric VanBuhler <eric@alwaysai.co>
2020-02-21 15:34:27 -0800

    Add CV client

--- cv/app.py
+++ cv/app.py
@@ -1,5 +1,7 @@
 import time
 import edgeiq
+import argparse
+import socketio
 """
 Use object detection to detect objects in the frame in realtime. The
 types of objects detected can be changed by selecting different models.
@@ -12,7 +14,28 @@ https://dashboard.alwaysai.co/docs/application_development/changing_the_engine_a
 """
 
 
-def main():
+class CVClient(object):
+    def __init__(self, server_addr):
+        self.server_addr = server_addr
+        self._sio = socketio.Client()
+
+    def setup(self):
+        print('[INFO] Connecting to server...')
+        self._sio.connect(
+                'http://{}:5000'.format(self.server_addr), namespaces=['/data'])
+        print('[INFO] Successfully connected to server.')
+        time.sleep(1)
+        return self
+
+    def send_data(self, frame, text):
+        # Process and send frame to web client
+        pass
+
+    def close(self):
+        self._sio.disconnect()
+
+
+def main(use_streamer, server_addr):
     obj_detect = edgeiq.ObjectDetection(
             "alwaysai/mobilenet_ssd")
     obj_detect.load(engine=edgeiq.Engine.DNN)
@@ -25,8 +48,12 @@ def main():
     fps = edgeiq.FPS()
 
     try:
-        with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
-                edgeiq.Streamer() as streamer:
+        if use_streamer:
+            streamer = edgeiq.Streamer().setup()
+        else:
+            streamer = CVClient(server_addr).setup()
+
+        with edgeiq.WebcamVideoStream(cam=0) as video_stream:
             # Allow Webcam to warm up
             time.sleep(2.0)
             fps.start()
@@ -56,6 +83,7 @@ def main():
                     break
 
     finally:
+        streamer.close()
         fps.stop()
         print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
         print("approx. FPS: {:.2f}".format(fps.compute_fps()))
@@ -64,4 +92,12 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Realtime Object Detector')
+    parser.add_argument(
+            '--use-streamer',  action='store_true',
+            help='Use the streamer instead of connecting to the server.')
+    parser.add_argument(
+            '--server_addr',  type=str, default='localhost',
+            help='The IP address or hostname of the SocketIO server.')
+    args = parser.parse_args()
+    main(args.use_streamer)
