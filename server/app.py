4ade29bdfbbfd6a6e44fdd1c35e69de93fcae653
Eric VanBuhler <eric@alwaysai.co>
2020-02-21 14:57:32 -0800

    Add socketIO client logs

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from flask_socketio import SocketIO
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -8,6 +8,14 @@ socketio = SocketIO(app)
 def index():
     """Home page."""
     return render_template('index.html')
+
+@socketio.on('connect')
+def connect():
+    print('[INFO] Client connected: {}'.format(request.sid))
+
+@socketio.on('disconnect')
+def disconnect():
+    print('[INFO] Client disconnected: {}'.format(request.sid))
 
 if __name__ == "__main__":
     socketio.run(app=app)
